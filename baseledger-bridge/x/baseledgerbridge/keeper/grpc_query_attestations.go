7b3d61e1f1183a60409737160c4947e715d74629
Stefan Kostic <skostic9242@gmail.com>
2022-01-16 10:08:02 +0100

    Add query attestations implementation

--- baseledger-bridge/x/baseledgerbridge/keeper/grpc_query_attestations.go
+++ baseledger-bridge/x/baseledgerbridge/keeper/grpc_query_attestations.go
@@ -9,15 +9,18 @@ import (
 	"google.golang.org/grpc/status"
 )
 
+const QUERY_ATTESTATIONS_LIMIT uint64 = 1000
+
 func (k Keeper) Attestations(goCtx context.Context, req *types.QueryAttestationsRequest) (*types.QueryAttestationsResponse, error) {
 	if req == nil {
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
 	}
-
 	ctx := sdk.UnwrapSDKContext(goCtx)
+	limit := req.Limit
+	if limit > QUERY_ATTESTATIONS_LIMIT {
+		limit = QUERY_ATTESTATIONS_LIMIT
+	}
+	attestations := k.GetMostRecentAttestations(ctx, limit)
 
-	// TODO: Process the query
-	_ = ctx
-
-	return &types.QueryAttestationsResponse{}, nil
+	return &types.QueryAttestationsResponse{Attestations: attestations}, nil
 }
