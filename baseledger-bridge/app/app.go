e551e02a02f4d9fc553031ed08e9daad4c0b4ae9
Stefan Kostic <skostic9242@gmail.com>
2022-01-25 14:10:11 +0100

    Handle ubt deposited claim

--- baseledger-bridge/app/app.go
+++ baseledger-bridge/app/app.go
@@ -367,6 +367,7 @@ func New(
 		&app.BankKeeper,
 		&app.DistrKeeper,
 		&stakingKeeper,
+		&app.AccountKeeper,
 	)
 	baseledgerbridgeModule := baseledgerbridgemodule.NewAppModule(appCodec, app.BaseledgerbridgeKeeper, app.AccountKeeper, app.BankKeeper)
 
