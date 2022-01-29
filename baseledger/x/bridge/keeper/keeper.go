98062b18ebe84f358ef548ee30adb14e1ac66ec0
Stefan Kostic <skostic9242@gmail.com>
2022-01-29 17:28:00 +0100

    Cleanup eth key from bridge module

--- baseledger/x/bridge/keeper/keeper.go
+++ baseledger/x/bridge/keeper/keeper.go
@@ -105,16 +105,6 @@ func (k Keeper) UnpackAttestationClaim(att *types.Attestation) (types.EthereumCl
 	}
 }
 
-// GetDelegateKeys iterates both the EthAddress and Orchestrator address indexes to produce
-// a vector of MsgSetOrchestratorAddress entires containing all the delgate keys for state
-// export / import. This may seem at first glance to be excessively complicated, why not combine
-// the EthAddress and Orchestrator address indexes and simply iterate one thing? The answer is that
-// even though we set the Eth and Orchestrator address in the same place we use them differently we
-// always go from Orchestrator address to Validator address and from validator address to Ethereum address
-// we want to keep looking up the validator address for various reasons, so a direct Orchestrator to Ethereum
-// address mapping will mean having to keep two of the same data around just to provide lookups.
-//
-// For the time being this will serve
 func (k Keeper) GetDelegateKeys(ctx sdk.Context) []types.MsgSetOrchestratorAddress {
 	store := ctx.KVStore(k.storeKey)
 
--- baseledger/x/bridge/types/key.go
+++ baseledger/x/bridge/types/key.go
@@ -22,14 +22,19 @@ var (
 	// LastObservedEventNonceKey indexes the latest event nonce
 	LastObservedEventNonceKey = "LastObservedEventNonceKey"
 
-	// EthAddressByValidatorKey indexes cosmos validator account addresses
-	// i.e. gravity1ahx7f8wyertuus9r20284ej0asrs085ceqtfnm
-	EthAddressByValidatorKey = "EthAddressValidatorKey"
-
-	// ValidatorByEthAddressKey indexes ethereum addresses
-	// i.e. 0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B
-	ValidatorByEthAddressKey = "ValidatorByEthAddressKey"
+	// KeyOrchestratorAddress indexes the validator keys for an orchestrator
+	KeyOrchestratorAddress = "KeyOrchestratorAddress"
 )
+
+// GetOrchestratorAddressKey returns the following key format
+// prefix
+// [0xe8][gravity1ahx7f8wyertuus9r20284ej0asrs085ceqtfnm]
+func GetOrchestratorAddressKey(orc sdk.AccAddress) string {
+	if err := sdk.VerifyAddressFormat(orc); err != nil {
+		panic(sdkerrors.Wrap(err, "invalid orchestrator address"))
+	}
+	return KeyOrchestratorAddress + string(orc.Bytes())
+}
 
 // GetAttestationKey returns the following key format
 // prefix     nonce                             claim-details-hash
@@ -55,23 +60,6 @@ func GetLastEventNonceByValidatorKey(validator sdk.ValAddress) string {
 		panic(sdkerrors.Wrap(err, "invalid validator address"))
 	}
 	return LastEventNonceByValidatorKey + string(validator.Bytes())
-}
-
-// GetEthAddressByValidatorKey returns the following key format
-// prefix              cosmos-validator
-// [0x0][gravityvaloper1ahx7f8wyertuus9r20284ej0asrs085ceqtfnm]
-func GetEthAddressByValidatorKey(validator sdk.ValAddress) string {
-	if err := sdk.VerifyAddressFormat(validator); err != nil {
-		panic(sdkerrors.Wrap(err, "invalid validator address"))
-	}
-	return EthAddressByValidatorKey + string(validator.Bytes())
-}
-
-// GetValidatorByEthAddressKey returns the following key format
-// prefix              cosmos-validator
-// [0xf9][0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B]
-func GetValidatorByEthAddressKey(ethAddress EthAddress) string {
-	return ValidatorByEthAddressKey + string([]byte(ethAddress.GetAddress()))
 }
 
 func convertByteArrToString(value []byte) string {
