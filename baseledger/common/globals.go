f4bb5781fcfcd44144560d7f63e0922049e69c44
Stefan Kostic <skostic9242@gmail.com>
2022-01-30 17:05:20 +0100

    Change faucet address example to baseledger address

--- baseledger/common/globals.go
+++ baseledger/common/globals.go
@@ -3,4 +3,4 @@ package common
 const StakeTokenDenom = "stake"
 const WorkTokenDenom = "work"
 const WorkTokenFee = "1work"
-const UbtFaucetAddress = "cosmos12s5kehx92laktvzzesf3dfs2hsg8gwfvjh0tfa"
+const UbtFaucetAddress = "baseledger1xgs5tamqre7rkz5q7d5fegjsdwufxxvt36w0a0"
