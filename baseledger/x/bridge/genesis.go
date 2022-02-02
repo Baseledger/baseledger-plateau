8481c46c78beb2d94a717322824f7fee5643f567
Stefan Kostic <skostic9242@gmail.com>
2022-02-02 13:04:32 +0100

    Implement init and export attestation genesis methods

--- baseledger/x/bridge/genesis.go
+++ baseledger/x/bridge/genesis.go
@@ -1,6 +1,8 @@
 package bridge
 
 import (
+	"fmt"
+
 	"github.com/Baseledger/baseledger/x/bridge/keeper"
 	"github.com/Baseledger/baseledger/x/bridge/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -11,12 +13,35 @@ import (
 func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) {
 	// this line is used by starport scaffolding # genesis/module/init
 	k.SetParams(ctx, genState.Params)
+
+	for _, att := range genState.Attestations {
+		att := att
+		claim, err := k.UnpackAttestationClaim(&att)
+		if err != nil {
+			panic("couldn't cast to claim")
+		}
+
+		// TODO: block height?
+		hash, err := claim.ClaimHash()
+		if err != nil {
+			panic(fmt.Errorf("error when computing ClaimHash for %v", hash))
+		}
+		k.SetAttestation(ctx, claim.GetEventNonce(), hash, &att)
+	}
 }
 
 // ExportGenesis returns the capability module's exported genesis.
 func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
 	genesis := types.DefaultGenesis()
 	genesis.Params = k.GetParams(ctx)
+
+	attestationMap, attestationKeys := k.GetAttestationMapping(ctx)
+
+	// export attestations from state
+	for _, key := range attestationKeys {
+		// TODO: set height = 0?
+		genesis.Attestations = append(genesis.Attestations, attestationMap[key]...)
+	}
 
 	// this line is used by starport scaffolding # genesis/module/export
 
