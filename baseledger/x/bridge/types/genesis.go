a879db86c497cc349dda84e9331a5bbf1366a552
Stefan Kostic <skostic9242@gmail.com>
2022-02-02 13:04:32 +0100

    Implement init and export attestation genesis methods

--- baseledger/x/bridge/types/genesis.go
+++ baseledger/x/bridge/types/genesis.go
@@ -18,7 +18,8 @@ var (
 func DefaultGenesis() *GenesisState {
 	return &GenesisState{
 		// this line is used by starport scaffolding # genesis/types/default
-		Params: DefaultParams(),
+		Params:       DefaultParams(),
+		Attestations: []Attestation{},
 	}
 }
 
