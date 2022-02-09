6451de3e15d78f322fb42ba19a4ccef8835b3898
Stefan Kostic <skostic9242@gmail.com>
2022-02-09 14:48:25 +0100

    Revert bad rebase

--- baseledger/x/bridge/keeper/orchestrator_validator_address_test.go
+++ baseledger/x/bridge/keeper/orchestrator_validator_address_test.go
@@ -19,6 +19,7 @@ func createNOrchestratorValidatorAddress(keeper *keeper.Keeper, ctx sdk.Context,
 	items := make([]types.OrchestratorValidatorAddress, n)
 	for i := range items {
 		items[i].OrchestratorAddress = strconv.Itoa(i)
+
 		keeper.SetOrchestratorValidatorAddress(ctx, items[i])
 	}
 	return items
