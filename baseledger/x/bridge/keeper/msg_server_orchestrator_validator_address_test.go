b8c835458d397b2686768d63b794cb0aa8ff3b20
Stefan Kostic <skostic9242@gmail.com>
2022-02-14 19:21:27 +0100

    Finish happy path test for create orch val msg

--- baseledger/x/bridge/keeper/msg_server_orchestrator_validator_address_test.go
+++ baseledger/x/bridge/keeper/msg_server_orchestrator_validator_address_test.go
@@ -1,35 +1,35 @@
 package keeper_test
 
 import (
-	"fmt"
 	"strconv"
 	"testing"
 
 	keepertest "github.com/Baseledger/baseledger/testutil/keeper"
 	"github.com/Baseledger/baseledger/x/bridge/keeper"
+	"github.com/Baseledger/baseledger/x/bridge/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
 )
 
 // Prevent strconv unused error
 var _ = strconv.IntSize
 
 func TestOrchestratorValidatorAddressMsgServerCreate(t *testing.T) {
-	testKeepers := keepertest.SetFiveValidators(t)
-	keeper.NewMsgServerImpl(*testKeepers.BridgeKeeper)
-	sdk.WrapSDKContext(testKeepers.Context)
+	testKeepers := keepertest.SetFiveValidators(t, false)
+	srv := keeper.NewMsgServerImpl(*testKeepers.BridgeKeeper)
+	wctx := sdk.WrapSDKContext(testKeepers.Context)
 	validatorAddress := testKeepers.StakingKeeper.GetValidators(testKeepers.Context, 10)[0].OperatorAddress
 
-	fmt.Printf("VALIDATOR ADDRESS %v\n", validatorAddress)
-	// for i := 0; i < 5; i++ {
-	// 	expected := &types.MsgCreateOrchestratorValidatorAddress{ValidatorAddress: validatorAddress,
-	// 		OrchestratorAddress: strconv.Itoa(i),
-	// 	}
-	// 	_, err := srv.CreateOrchestratorValidatorAddress(wctx, expected)
-	// 	require.NoError(t, err)
-	// 	rst, found := k.GetOrchestratorValidatorAddress(ctx,
-	// 		expected.OrchestratorAddress,
-	// 	)
-	// 	require.True(t, found)
-	// 	require.Equal(t, expected.ValidatorAddress, rst.ValidatorAddress)
-	// }
+	msg := &types.MsgCreateOrchestratorValidatorAddress{
+		ValidatorAddress:    validatorAddress,
+		OrchestratorAddress: keepertest.AccAddrs[0].String(),
+	}
+
+	_, err := srv.CreateOrchestratorValidatorAddress(wctx, msg)
+	require.NoError(t, err)
+	rst, found := testKeepers.BridgeKeeper.GetOrchestratorValidatorAddress(testKeepers.Context,
+		msg.OrchestratorAddress,
+	)
+	require.True(t, found)
+	require.Equal(t, msg.ValidatorAddress, rst.ValidatorAddress)
 }
