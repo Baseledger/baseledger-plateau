80950f4025c501012673d4177878b074e79a8490
ognjenkurtic <kurticognjen@gmail.com>
2022-02-14 21:27:58 +0100

    WIP port buildcontainers

---
+++ tests/build-container.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -eux
+
+# this directy of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DOCKERFOLDER=$DIR/dockerfile
+REPOFOLDER=$DIR/..
+
+# change our directory sot hat the git arcive command works as expected
+pushd $REPOFOLDER
+
+#docker system prune -a -f
+# Build base container
+git archive --format=tar.gz -o $DOCKERFOLDER/baseledger.tar.gz --prefix=baseledger/ HEAD
+pushd $DOCKERFOLDER
+
+# setup for Mac M1 Compatibility 
+PLATFORM_CMD=""
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    if [[ -n $(sysctl -a | grep brand | grep "M1") ]]; then
+       echo "Setting --platform=linux/amd64 for Mac M1 compatibility"
+       PLATFORM_CMD="--platform=linux/amd64"; fi
+fi
+docker build -t baseledger-base $PLATFORM_CMD .
