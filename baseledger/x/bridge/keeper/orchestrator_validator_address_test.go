d95c5a64b1b0ee3643df6fdbd756abeaa6283365
Stefan Kostic <skostic9242@gmail.com>
2022-02-17 23:57:42 +0100

    Add tests for get orch validator

--- baseledger/x/bridge/keeper/orchestrator_validator_address_test.go
+++ baseledger/x/bridge/keeper/orchestrator_validator_address_test.go
@@ -1,49 +1,60 @@
 package keeper_test
 
-// import (
-// 	"strconv"
-// 	"testing"
+import (
+	"testing"
 
-// 	keepertest "github.com/Baseledger/baseledger/testutil/keeper"
-// 	"github.com/Baseledger/baseledger/testutil/nullify"
-// 	"github.com/Baseledger/baseledger/x/bridge/keeper"
-// 	"github.com/Baseledger/baseledger/x/bridge/types"
-// 	sdk "github.com/cosmos/cosmos-sdk/types"
-// 	"github.com/stretchr/testify/require"
-// )
+	keepertest "github.com/Baseledger/baseledger/testutil/keeper"
+	"github.com/Baseledger/baseledger/x/bridge/types"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+)
 
-// // Prevent strconv unused error
-// var _ = strconv.IntSize
+func TestGetOrchestratorValidator_GetSuccessful(t *testing.T) {
+	testKeepers := keepertest.SetFiveValidators(t, true)
 
-// func createNOrchestratorValidatorAddress(keeper *keeper.Keeper, ctx sdk.Context, n int) []types.OrchestratorValidatorAddress {
-// 	items := make([]types.OrchestratorValidatorAddress, n)
-// 	for i := range items {
-// 		items[i].OrchestratorAddress = strconv.Itoa(i)
+	val := testKeepers.BridgeKeeper.GetOrchestratorValidator(testKeepers.Context, keepertest.OrchAddrs[0].String())
+	require.NotNil(t, val)
+}
 
-// 		keeper.SetOrchestratorValidatorAddress(ctx, items[i])
-// 	}
-// 	return items
-// }
+func TestGetOrchestratorValidator_GetFailWrongOrchestratorAddressFormat(t *testing.T) {
+	testKeepers := keepertest.SetFiveValidators(t, true)
 
-// func TestOrchestratorValidatorAddressGet(t *testing.T) {
-// 	keeper, ctx := keepertest.BaseledgerbridgeKeeper(t)
-// 	items := createNOrchestratorValidatorAddress(keeper, ctx, 10)
-// 	for _, item := range items {
-// 		rst, found := keeper.GetOrchestratorValidatorAddress(ctx,
-// 			item.OrchestratorAddress,
-// 		)
-// 		require.True(t, found)
-// 		require.Equal(t,
-// 			nullify.Fill(&item),
-// 			nullify.Fill(&rst),
-// 		)
-// 	}
-// }
-// func TestOrchestratorValidatorAddressGetAll(t *testing.T) {
-// 	keeper, ctx := keepertest.BaseledgerbridgeKeeper(t)
-// 	items := createNOrchestratorValidatorAddress(keeper, ctx, 10)
-// 	require.ElementsMatch(t,
-// 		nullify.Fill(items),
-// 		nullify.Fill(keeper.GetAllOrchestratorValidatorAddresses(ctx)),
-// 	)
-// }
+	val := testKeepers.BridgeKeeper.GetOrchestratorValidator(testKeepers.Context, "12345")
+	require.Nil(t, val)
+}
+
+func TestGetOrchestratorValidator_GetFailWrongOrchestratorAddress(t *testing.T) {
+	testKeepers := keepertest.SetFiveValidators(t, false)
+
+	val := testKeepers.BridgeKeeper.GetOrchestratorValidator(testKeepers.Context, keepertest.OrchAddrs[0].String())
+	require.Nil(t, val)
+}
+
+func TestGetOrchestratorValidator_GetFailWrongValidatorAddressFormat(t *testing.T) {
+	testKeepers := keepertest.SetFiveValidators(t, false)
+	// this will never happen because of validate basic, but just to cover the case
+	testKeepers.BridgeKeeper.SetOrchestratorValidatorAddress(testKeepers.Context, types.OrchestratorValidatorAddress{
+		OrchestratorAddress: keepertest.OrchAddrs[0].String(),
+		ValidatorAddress:    "12345",
+	})
+
+	val := testKeepers.BridgeKeeper.GetOrchestratorValidator(testKeepers.Context, keepertest.OrchAddrs[0].String())
+	require.Nil(t, val)
+}
+
+func TestGetOrchestratorValidator_GetFailWrongValidatorAddress(t *testing.T) {
+	testKeepers := keepertest.SetFiveValidators(t, false)
+	// generate valid validator address for not existing validator
+	AccPrivKey := secp256k1.GenPrivKey()
+	AccPubKey := AccPrivKey.PubKey()
+	ValAddr := sdk.ValAddress(AccPubKey.Address())
+	// this will never happen because of validate basic, but just to cover the case
+	testKeepers.BridgeKeeper.SetOrchestratorValidatorAddress(testKeepers.Context, types.OrchestratorValidatorAddress{
+		OrchestratorAddress: keepertest.OrchAddrs[0].String(),
+		ValidatorAddress:    ValAddr.String(),
+	})
+
+	val := testKeepers.BridgeKeeper.GetOrchestratorValidator(testKeepers.Context, keepertest.OrchAddrs[0].String())
+	require.Nil(t, val)
+}
