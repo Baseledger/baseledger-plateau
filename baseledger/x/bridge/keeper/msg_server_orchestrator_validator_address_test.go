f15a598f40232bd7bef4b4560623f2916dc408ce
Stefan Kostic <skostic9242@gmail.com>
2022-02-17 23:30:05 +0100

    Add failing tests for msg server orch val address

--- baseledger/x/bridge/keeper/msg_server_orchestrator_validator_address_test.go
+++ baseledger/x/bridge/keeper/msg_server_orchestrator_validator_address_test.go
@@ -7,6 +7,7 @@ import (
 	keepertest "github.com/Baseledger/baseledger/testutil/keeper"
 	"github.com/Baseledger/baseledger/x/bridge/keeper"
 	"github.com/Baseledger/baseledger/x/bridge/types"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
 )
@@ -14,7 +15,7 @@ import (
 // Prevent strconv unused error
 var _ = strconv.IntSize
 
-func TestOrchestratorValidatorAddressMsgServerCreate(t *testing.T) {
+func TestOrchestratorValidatorAddress_CreateSuccess(t *testing.T) {
 	testKeepers := keepertest.SetFiveValidators(t, false)
 	srv := keeper.NewMsgServerImpl(*testKeepers.BridgeKeeper)
 	wctx := sdk.WrapSDKContext(testKeepers.Context)
@@ -32,4 +33,73 @@ func TestOrchestratorValidatorAddressMsgServerCreate(t *testing.T) {
 	)
 	require.True(t, found)
 	require.Equal(t, msg.ValidatorAddress, rst.ValidatorAddress)
+}
+
+func TestOrchestratorValidatorAddress_OrchestratorAlreadySet(t *testing.T) {
+	// flag true will also set orch validators for all 5 validators
+	testKeepers := keepertest.SetFiveValidators(t, true)
+	srv := keeper.NewMsgServerImpl(*testKeepers.BridgeKeeper)
+	wctx := sdk.WrapSDKContext(testKeepers.Context)
+	validatorAddress := testKeepers.StakingKeeper.GetValidators(testKeepers.Context, 10)[0].OperatorAddress
+
+	msg := &types.MsgCreateOrchestratorValidatorAddress{
+		ValidatorAddress:    validatorAddress,
+		OrchestratorAddress: keepertest.OrchAddrs[0].String(),
+	}
+	_, err := srv.CreateOrchestratorValidatorAddress(wctx, msg)
+	require.Error(t, err)
+	require.Equal(t, "orchestrator already set: invalid request", err.Error())
+}
+
+func TestOrchestratorValidatorAddress_ValidatorAlreadySet(t *testing.T) {
+	// flag true will also set orch validators for all 5 validators
+	testKeepers := keepertest.SetFiveValidators(t, true)
+	srv := keeper.NewMsgServerImpl(*testKeepers.BridgeKeeper)
+	wctx := sdk.WrapSDKContext(testKeepers.Context)
+	validatorAddress := testKeepers.StakingKeeper.GetValidators(testKeepers.Context, 10)[0].OperatorAddress
+
+	msg := &types.MsgCreateOrchestratorValidatorAddress{
+		ValidatorAddress:    validatorAddress,
+		OrchestratorAddress: keepertest.AccAddrs[0].String(),
+	}
+	_, err := srv.CreateOrchestratorValidatorAddress(wctx, msg)
+	require.Error(t, err)
+	require.Equal(t, "validator already set: invalid request", err.Error())
+}
+
+func TestOrchestratorValidatorAddress_InvalidValidatorAddress(t *testing.T) {
+	// flag true will also set orch validators for all 5 validators
+	testKeepers := keepertest.SetFiveValidators(t, true)
+	srv := keeper.NewMsgServerImpl(*testKeepers.BridgeKeeper)
+	wctx := sdk.WrapSDKContext(testKeepers.Context)
+	// validatorAddress := testKeepers.StakingKeeper.GetValidators(testKeepers.Context, 10)[0].OperatorAddress
+
+	msg := &types.MsgCreateOrchestratorValidatorAddress{
+		ValidatorAddress:    "12345",
+		OrchestratorAddress: keepertest.AccAddrs[0].String(),
+	}
+	_, err := srv.CreateOrchestratorValidatorAddress(wctx, msg)
+	require.Error(t, err)
+	require.Equal(t, "validator address format error: not found", err.Error())
+}
+
+func TestOrchestratorValidatorAddress_ValidatorNotFound(t *testing.T) {
+	// generate valid validator address for not existing validator
+	AccPrivKey := secp256k1.GenPrivKey()
+	AccPubKey := AccPrivKey.PubKey()
+	ValAddr := sdk.ValAddress(AccPubKey.Address())
+
+	// flag true will also set orch validators for all 5 validators
+	testKeepers := keepertest.SetFiveValidators(t, true)
+	srv := keeper.NewMsgServerImpl(*testKeepers.BridgeKeeper)
+	wctx := sdk.WrapSDKContext(testKeepers.Context)
+	// validatorAddress := testKeepers.StakingKeeper.GetValidators(testKeepers.Context, 10)[0].OperatorAddress
+
+	msg := &types.MsgCreateOrchestratorValidatorAddress{
+		ValidatorAddress:    ValAddr.String(),
+		OrchestratorAddress: keepertest.AccAddrs[0].String(),
+	}
+	_, err := srv.CreateOrchestratorValidatorAddress(wctx, msg)
+	require.Error(t, err)
+	require.Equal(t, "validator not found or not in active set: not found", err.Error())
 }
