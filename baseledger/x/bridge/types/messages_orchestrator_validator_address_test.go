fcbadd37104226e1d289819441818c46d8990057
Stefan Kostic <skostic9242@gmail.com>
2022-02-19 20:43:19 +0100

    Test validate basic orch val address

--- baseledger/x/bridge/types/messages_orchestrator_validator_address_test.go
+++ baseledger/x/bridge/types/messages_orchestrator_validator_address_test.go
@@ -4,34 +4,42 @@ import (
 	"testing"
 
 	"github.com/Baseledger/baseledger/testutil/sample"
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/stretchr/testify/require"
 )
 
 func TestMsgCreateOrchestratorValidatorAddress_ValidateBasic(t *testing.T) {
 	tests := []struct {
-		name string
-		msg  MsgCreateOrchestratorValidatorAddress
-		err  error
+		name          string
+		msg           MsgCreateOrchestratorValidatorAddress
+		isErrExpected bool
 	}{
 		{
-			name: "invalid address",
+			name: "invalid val address",
 			msg: MsgCreateOrchestratorValidatorAddress{
 				ValidatorAddress: "invalid_address",
 			},
-			err: sdkerrors.ErrInvalidAddress,
+			isErrExpected: true,
 		}, {
-			name: "valid address",
+			name: "invalid orch address",
 			msg: MsgCreateOrchestratorValidatorAddress{
-				ValidatorAddress: sample.AccAddress(),
+				ValidatorAddress:    sample.ValAddress(),
+				OrchestratorAddress: "invalid_address",
 			},
+			isErrExpected: true,
+		}, {
+			name: "valid",
+			msg: MsgCreateOrchestratorValidatorAddress{
+				ValidatorAddress:    sample.ValAddress(),
+				OrchestratorAddress: sample.AccAddress(),
+			},
+			isErrExpected: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			err := tt.msg.ValidateBasic()
-			if tt.err != nil {
-				require.ErrorIs(t, err, tt.err)
+			if tt.isErrExpected {
+				require.Error(t, err)
 				return
 			}
 			require.NoError(t, err)
