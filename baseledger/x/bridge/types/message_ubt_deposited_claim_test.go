f7fef0cd5b595f0bd87adf2c869bc1e7eb331b9a
Stefan Kostic <skostic9242@gmail.com>
2022-02-19 20:37:00 +0100

    Test ubt claim validate basic

--- baseledger/x/bridge/types/message_ubt_deposited_claim_test.go
+++ baseledger/x/bridge/types/message_ubt_deposited_claim_test.go
@@ -4,34 +4,77 @@ import (
 	"testing"
 
 	"github.com/Baseledger/baseledger/testutil/sample"
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/stretchr/testify/require"
 )
 
 func TestMsgUbtDepositedClaim_ValidateBasic(t *testing.T) {
 	tests := []struct {
-		name string
-		msg  MsgUbtDepositedClaim
-		err  error
+		name          string
+		msg           MsgUbtDepositedClaim
+		isErrExpected bool
 	}{
 		{
-			name: "invalid address",
+			name: "invalid eth sender",
 			msg: MsgUbtDepositedClaim{
-				Creator: "invalid_address",
+				EthereumSender: "invalid_eth_sender",
 			},
-			err: sdkerrors.ErrInvalidAddress,
-		}, {
-			name: "valid address",
+			isErrExpected: true,
+		},
+		{
+			name: "invalid token contract",
 			msg: MsgUbtDepositedClaim{
-				Creator: sample.AccAddress(),
+				EthereumSender: "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266",
+				TokenContract:  "invalid_token_contract",
 			},
+			isErrExpected: true,
+		},
+		{
+			name: "invalid orch address",
+			msg: MsgUbtDepositedClaim{
+				EthereumSender: "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266",
+				TokenContract:  "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+				Creator:        "invalid_creator",
+			},
+			isErrExpected: true,
+		},
+		{
+			name: "invalid orch address",
+			msg: MsgUbtDepositedClaim{
+				EthereumSender:                   "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266",
+				TokenContract:                    "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+				Creator:                          sample.AccAddress(),
+				BaseledgerReceiverAccountAddress: "invalid_receiver",
+			},
+			isErrExpected: true,
+		},
+		{
+			name: "invalid event nonce",
+			msg: MsgUbtDepositedClaim{
+				EthereumSender:                   "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266",
+				TokenContract:                    "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+				Creator:                          sample.AccAddress(),
+				BaseledgerReceiverAccountAddress: sample.AccAddress(),
+				EventNonce:                       0,
+			},
+			isErrExpected: true,
+		},
+		{
+			name: "valid",
+			msg: MsgUbtDepositedClaim{
+				EthereumSender:                   "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266",
+				TokenContract:                    "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+				Creator:                          sample.AccAddress(),
+				BaseledgerReceiverAccountAddress: sample.AccAddress(),
+				EventNonce:                       1,
+			},
+			isErrExpected: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			err := tt.msg.ValidateBasic()
-			if tt.err != nil {
-				require.ErrorIs(t, err, tt.err)
+			if tt.isErrExpected {
+				require.Error(t, err)
 				return
 			}
 			require.NoError(t, err)
