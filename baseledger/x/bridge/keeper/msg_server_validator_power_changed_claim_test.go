78404f98b2638462920a436c733d20e1a3c8994f
Stefan Kostic <skostic9242@gmail.com>
2022-02-19 17:31:28 +0100

    Add tests for non registered validators

--- baseledger/x/bridge/keeper/msg_server_validator_power_changed_claim_test.go
+++ baseledger/x/bridge/keeper/msg_server_validator_power_changed_claim_test.go
@@ -110,3 +110,45 @@ func TestValidatorPowerChangedClaim(t *testing.T) {
 	validator, _ = testKeepers.StakingKeeper.GetValidator(ctx, validatorReceiver)
 	require.Equal(t, validator.Tokens, newAmount)
 }
+
+func TestValidatorPowerChangedClaim_NonRegisteredOrchestratorValidator(t *testing.T) {
+	var (
+		baseledgerTokenContract = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+		ethereumSender          = "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266"
+		myBlockTime             = time.Date(2020, 9, 14, 15, 20, 10, 0, time.UTC)
+	)
+	// DON'T register validators orchestrators
+	testKeepers := keepertest.SetFiveValidators(t, false)
+
+	validatorReceiver := keepertest.ValAddrs[0]
+	ctx := testKeepers.Context
+
+	validator, _ := testKeepers.StakingKeeper.GetValidator(ctx, validatorReceiver)
+	startAmount, _ := sdk.NewIntFromString("10000000")
+	require.Equal(t, validator.Tokens, startAmount)
+
+	srv := keeper.NewMsgServerImpl(*testKeepers.BridgeKeeper)
+
+	// all validators, nonce 1
+	newAmount, _ := sdk.NewIntFromString("10000005")
+	for _, orchAddress := range keepertest.OrchAddrs {
+		claim := types.MsgValidatorPowerChangedClaim{
+			Creator:                            orchAddress.String(),
+			EventNonce:                         uint64(1),
+			TokenContract:                      baseledgerTokenContract,
+			Amount:                             newAmount,
+			BaseledgerReceiverValidatorAddress: validatorReceiver.String(),
+			RevenueAddress:                     ethereumSender,
+		}
+
+		ctx = ctx.WithBlockTime(myBlockTime)
+		_, err := srv.ValidatorPowerChangedClaim(sdk.WrapSDKContext(ctx), &claim)
+		bridge.EndBlocker(ctx, *testKeepers.BridgeKeeper)
+		require.Error(t, err)
+
+		hash, err := claim.ClaimHash()
+		require.NoError(t, err)
+		attestation := testKeepers.BridgeKeeper.GetAttestation(ctx, uint64(1), hash)
+		require.Nil(t, attestation)
+	}
+}
