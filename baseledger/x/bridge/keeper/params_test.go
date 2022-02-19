78bb4f782d9fea5c6b0a1164d09e88a294cd09d7
Stefan Kostic <skostic9242@gmail.com>
2022-02-19 20:18:13 +0100

    Fix test get bridge params

--- baseledger/x/bridge/keeper/params_test.go
+++ baseledger/x/bridge/keeper/params_test.go
@@ -1,18 +1,21 @@
 package keeper_test
 
-// import (
-// 	"testing"
+import (
+	"testing"
 
-// 	testkeeper "github.com/Baseledger/baseledger/testutil/keeper"
-// 	"github.com/Baseledger/baseledger/x/bridge/types"
-// 	"github.com/stretchr/testify/require"
-// )
+	keepertest "github.com/Baseledger/baseledger/testutil/keeper"
+	"github.com/Baseledger/baseledger/x/bridge/types"
+	"github.com/stretchr/testify/require"
+)
 
-// func TestGetParams(t *testing.T) {
-// 	k, ctx := testkeeper.BaseledgerbridgeKeeper(t)
-// 	params := types.DefaultParams()
+func TestGetParams(t *testing.T) {
+	testKeepers := keepertest.SetFiveValidators(t, true)
+	params := types.Params{
+		WorktokenEurPrice:       "0.1",
+		BaseledgerFaucetAddress: keepertest.FaucetAccount.String(),
+	}
 
-// 	k.SetParams(ctx, params)
+	testKeepers.BridgeKeeper.SetParams(testKeepers.Context, params)
 
-// 	require.EqualValues(t, params, k.GetParams(ctx))
-// }
+	require.EqualValues(t, params, testKeepers.BridgeKeeper.GetParams(testKeepers.Context))
+}
