9f4ee116927c8df28d2c8900a1c8a6858515cbe2
Stefan Kostic <skostic9242@gmail.com>
2022-02-19 20:58:00 +0100

    Test bridge genesis state init and export

--- baseledger/x/bridge/genesis_test.go
+++ baseledger/x/bridge/genesis_test.go
@@ -1,38 +1,67 @@
 package bridge_test
 
-// import (
-// 	"testing"
+import (
+	"testing"
 
-// 	keepertest "github.com/Baseledger/baseledger/testutil/keeper"
-// 	"github.com/Baseledger/baseledger/testutil/nullify"
-// 	bridge "github.com/Baseledger/baseledger/x/bridge"
-// 	"github.com/Baseledger/baseledger/x/bridge/types"
-// 	"github.com/stretchr/testify/require"
-// )
+	keepertest "github.com/Baseledger/baseledger/testutil/keeper"
+	"github.com/Baseledger/baseledger/testutil/nullify"
+	"github.com/Baseledger/baseledger/testutil/sample"
+	bridge "github.com/Baseledger/baseledger/x/bridge"
+	"github.com/Baseledger/baseledger/x/bridge/types"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+)
 
-// func TestGenesis(t *testing.T) {
-// 	genesisState := types.GenesisState{
-// 		Params: types.DefaultParams(),
+func TestGenesis(t *testing.T) {
+	genesisState := types.GenesisState{
+		Params: types.Params{
+			WorktokenEurPrice:       "0.1",
+			BaseledgerFaucetAddress: sample.AccAddress(),
+		},
 
-// 		OrchestratorValidatorAddressList: []types.OrchestratorValidatorAddress{
-// 		{
-// 			OrchestratorAddress: "0",
-// },
-// 		{
-// 			OrchestratorAddress: "1",
-// },
-// 	},
-// 	// this line is used by starport scaffolding # genesis/test/state
-// 	}
+		OrchestratorValidatorAddressList: []types.OrchestratorValidatorAddress{
+			{
+				OrchestratorAddress: sample.AccAddress(),
+				ValidatorAddress:    sample.ValAddress(),
+			},
+			{
+				OrchestratorAddress: sample.AccAddress(),
+				ValidatorAddress:    sample.ValAddress(),
+			},
+		},
 
-// 	k, ctx := keepertest.BaseledgerbridgeKeeper(t)
-// 	bridge.InitGenesis(ctx, *k, genesisState)
-// 	got := bridge.ExportGenesis(ctx, *k)
-// 	require.NotNil(t, got)
+		LastObservedNonce: 5,
 
-// 	nullify.Fill(&genesisState)
-// 	nullify.Fill(got)
+		Attestations: []types.Attestation{
+			{
+				Observed: true,
+				Votes:    []string{sample.ValAddress()},
+				Height:   uint64(2),
+				Claim: &codectypes.Any{
+					TypeUrl:              "/Baseledger.baseledger.bridge.MsgValidatorPowerChangedClaim",
+					Value:                []byte{},
+					XXX_NoUnkeyedLiteral: struct{}{},
+					XXX_unrecognized:     []byte{},
+					XXX_sizecache:        0,
+				},
+				UbtPrices: []sdk.Int{sdk.NewIntFromUint64(1), sdk.NewIntFromUint64(2)},
+			},
+		},
+		// this line is used by starport scaffolding # genesis/test/state
+	}
 
-// 	require.ElementsMatch(t, genesisState.OrchestratorValidatorAddressList, got.OrchestratorValidatorAddressList)
-// // this line is used by starport scaffolding # genesis/test/assert
-// }
+	testKeepers := keepertest.BaseledgerbridgeKeeper(t)
+	bridge.InitGenesis(testKeepers.Context, *testKeepers.BridgeKeeper, genesisState)
+	got := bridge.ExportGenesis(testKeepers.Context, *testKeepers.BridgeKeeper)
+	require.NotNil(t, got)
+
+	nullify.Fill(&genesisState)
+	nullify.Fill(got)
+
+	require.ElementsMatch(t, genesisState.OrchestratorValidatorAddressList, got.OrchestratorValidatorAddressList)
+	require.ElementsMatch(t, genesisState.Attestations, got.Attestations)
+
+	require.Equal(t, uint64(5), got.LastObservedNonce)
+	// this line is used by starport scaffolding # genesis/test/assert
+}
