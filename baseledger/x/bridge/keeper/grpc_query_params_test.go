795ace6611382299e7802288fd9b3e9883ffc0c3
Stefan Kostic <skostic9242@gmail.com>
2022-02-19 21:02:29 +0100

    Cleanup

--- baseledger/x/bridge/keeper/grpc_query_params_test.go
+++ baseledger/x/bridge/keeper/grpc_query_params_test.go
@@ -1,21 +1,25 @@
 package keeper_test
 
-// import (
-// 	"testing"
+import (
+	"testing"
 
-// 	testkeeper "github.com/Baseledger/baseledger/testutil/keeper"
-// 	"github.com/Baseledger/baseledger/x/bridge/types"
-// 	sdk "github.com/cosmos/cosmos-sdk/types"
-// 	"github.com/stretchr/testify/require"
-// )
+	testkeeper "github.com/Baseledger/baseledger/testutil/keeper"
+	"github.com/Baseledger/baseledger/testutil/sample"
+	"github.com/Baseledger/baseledger/x/bridge/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+)
 
-// func TestParamsQuery(t *testing.T) {
-// 	keeper, ctx := testkeeper.BaseledgerbridgeKeeper(t)
-// 	wctx := sdk.WrapSDKContext(ctx)
-// 	params := types.DefaultParams()
-// 	keeper.SetParams(ctx, params)
+func TestParamsQuery(t *testing.T) {
+	testKeepers := testkeeper.BaseledgerbridgeKeeper(t)
+	wctx := sdk.WrapSDKContext(testKeepers.Context)
+	params := types.Params{
+		BaseledgerFaucetAddress: sample.AccAddress(),
+		WorktokenEurPrice:       "0.1",
+	}
+	testKeepers.BridgeKeeper.SetParams(testKeepers.Context, params)
 
-// 	response, err := keeper.Params(wctx, &types.QueryParamsRequest{})
-// 	require.NoError(t, err)
-// 	require.Equal(t, &types.QueryParamsResponse{Params: params}, response)
-// }
+	response, err := testKeepers.BridgeKeeper.Params(wctx, &types.QueryParamsRequest{})
+	require.NoError(t, err)
+	require.Equal(t, &types.QueryParamsResponse{Params: params}, response)
+}
