b2f4c7e456e5a46fda19e0c7d24e23d0ee5519eb
ognjenkurtic <kurticognjen@gmail.com>
2022-02-21 00:13:50 +0100

    Rename script

--- tests/start-containers.sh
+++ tests/start-containers.sh
@@ -29,7 +29,6 @@ fi
 
 # Run new test container instances
 
-## TODO: We might be missing ports for P2P - check if fails to discover other nodes
 GRPC_PORT=9090
 RPC_PORT=26657
 API_PORT=1317
@@ -43,14 +42,8 @@ for i in $(seq 1 $NODES);
 do
 
 # add this ip for loopback dialing
+# ip addr add 7.7.7.$i/32 dev eth0 || true # allowed to fail
 
-ip addr add 7.7.7.$i/32 dev eth0 || true # allowed to fail
-docker run --name $VALIDATOR_CONTAINER_BASE_NAME$i $PLATFORM_CMD --cap-add=NET_ADMIN --net baseledgernet -d -p $CONTAINER_IP$i:$GRPC_PORT:9090 -p $CONTAINER_IP$i:$RPC_PORT:26657 -p $CONTAINER_IP$i:$API_PORT:1317 -p $CONTAINER_IP$i:$LISTEN_PORT:26655 -p $CONTAINER_IP$i:$P2P_PORT:26656 baseledger-base
-
-# GRPC_PORT=$(($GRPC_PORT + 1))
-# RPC_PORT=$(($RPC_PORT + 1))
-# API_PORT=$(($API_PORT + 1))
-# LISTEN_PORT=$(($LISTEN_PORT + 10))
-# P2P_PORT=$(($P2P_PORT + 10))
+docker run --name $VALIDATOR_CONTAINER_BASE_NAME$i $PLATFORM_CMD --cap-add=NET_ADMIN --net baseledgernet -d --expose 9090 --expose 26657 --expose 1317 --expose 26655 --expose 26656 baseledger-base
 
 done
