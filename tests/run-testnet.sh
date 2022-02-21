8ffca1b43bb73f453a2137bbe9a997a44bf6bbd1
ognjenkurtic <kurticognjen@gmail.com>
2022-02-21 21:50:46 +0100

    Add node 1 as persisted peer to all nodes to enable discoverability

--- tests/run-testnet.sh
+++ tests/run-testnet.sh
@@ -3,11 +3,17 @@ set -eux
 # your gaiad binary name
 BIN=baseledgerd
 VALIDATOR_CONTAINER_BASE_NAME="baseledger-validator-container"
+BASELEDGER_HOME="--home /validator"
 NODES=3
+
+# We are adding first validator as a persisted peer since we could not get pex to autodiscover with this setup
+# (might be an issue with "Cannot add non-routable address fd010b69bae8fb323d9527e377497b93608c11f8@172.24.0.2:26656)
+# check by relaxing requirement for routability
+FIRST_VALIDATOR_NODE_ID=$(docker exec $VALIDATOR_CONTAINER_BASE_NAME"1" baseledgerd $BASELEDGER_HOME tendermint show-node-id)
+FIRST_VALIDATOR_CONTAINER_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $VALIDATOR_CONTAINER_BASE_NAME"1")
 
 for i in $(seq 1 $NODES);
 do
-    BASELEDGER_HOME="--home /validator"
     # this implicitly caps us at ~6000 nodes for this sim
     # note that we start on 26656 the idea here is that the first
     # node (node 1) is at the expected contact address from the gentx
@@ -16,9 +22,10 @@ do
     GRPC_ADDRESS="--grpc.address 0.0.0.0:9090"
     LISTEN_ADDRESS="--address tcp://0.0.0.0:26655"
     P2P_ADDRESS="--p2p.laddr tcp://0.0.0.0:26656"
+    PERSISTENT_PEERS="--p2p.persistent_peers $FIRST_VALIDATOR_NODE_ID@$FIRST_VALIDATOR_CONTAINER_IP:26656"
     LOG_LEVEL="--log_level info"
     INVARIANTS_CHECK="--inv-check-period 1"
-    ARGS="$BASELEDGER_HOME $LISTEN_ADDRESS $RPC_ADDRESS $GRPC_ADDRESS $LOG_LEVEL $INVARIANTS_CHECK $P2P_ADDRESS"
+    ARGS="$BASELEDGER_HOME $LISTEN_ADDRESS $RPC_ADDRESS $GRPC_ADDRESS $LOG_LEVEL $INVARIANTS_CHECK $P2P_ADDRESS $PERSISTENT_PEERS"
     rm -rf /validator$i
     mkdir /validator$i && touch /validator$i/logs
     docker exec $VALIDATOR_CONTAINER_BASE_NAME$i $BIN $ARGS start &> /validator$i/logs &
