cc3966f86a74243b43618539a18af950b510203e
Stefan Kostic <skostic9242@gmail.com>
2022-02-21 11:45:40 +0100

    Remove update and delete baseledger transaction

--- baseledger/x/proof/keeper/msg_server_baseledger_transaction_test.go
+++ baseledger/x/proof/keeper/msg_server_baseledger_transaction_test.go
@@ -3,7 +3,6 @@ package keeper_test
 import (
 	"testing"
 
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/stretchr/testify/require"
 
 	"github.com/Baseledger/baseledger/x/proof/types"
@@ -16,81 +15,5 @@ func TestBaseledgerTransactionMsgServerCreate(t *testing.T) {
 		resp, err := srv.CreateBaseledgerTransaction(ctx, &types.MsgCreateBaseledgerTransaction{Creator: creator})
 		require.NoError(t, err)
 		require.Equal(t, i, int(resp.Id))
-	}
-}
-
-func TestBaseledgerTransactionMsgServerUpdate(t *testing.T) {
-	creator := "A"
-
-	for _, tc := range []struct {
-		desc    string
-		request *types.MsgUpdateBaseledgerTransaction
-		err     error
-	}{
-		{
-			desc:    "Completed",
-			request: &types.MsgUpdateBaseledgerTransaction{Creator: creator},
-		},
-		{
-			desc:    "Unauthorized",
-			request: &types.MsgUpdateBaseledgerTransaction{Creator: "B"},
-			err:     sdkerrors.ErrUnauthorized,
-		},
-		{
-			desc:    "Unauthorized",
-			request: &types.MsgUpdateBaseledgerTransaction{Creator: creator, Id: 10},
-			err:     sdkerrors.ErrKeyNotFound,
-		},
-	} {
-		t.Run(tc.desc, func(t *testing.T) {
-			srv, ctx := setupMsgServer(t)
-			_, err := srv.CreateBaseledgerTransaction(ctx, &types.MsgCreateBaseledgerTransaction{Creator: creator})
-			require.NoError(t, err)
-
-			_, err = srv.UpdateBaseledgerTransaction(ctx, tc.request)
-			if tc.err != nil {
-				require.ErrorIs(t, err, tc.err)
-			} else {
-				require.NoError(t, err)
-			}
-		})
-	}
-}
-
-func TestBaseledgerTransactionMsgServerDelete(t *testing.T) {
-	creator := "A"
-
-	for _, tc := range []struct {
-		desc    string
-		request *types.MsgDeleteBaseledgerTransaction
-		err     error
-	}{
-		{
-			desc:    "Completed",
-			request: &types.MsgDeleteBaseledgerTransaction{Creator: creator},
-		},
-		{
-			desc:    "Unauthorized",
-			request: &types.MsgDeleteBaseledgerTransaction{Creator: "B"},
-			err:     sdkerrors.ErrUnauthorized,
-		},
-		{
-			desc:    "KeyNotFound",
-			request: &types.MsgDeleteBaseledgerTransaction{Creator: creator, Id: 10},
-			err:     sdkerrors.ErrKeyNotFound,
-		},
-	} {
-		t.Run(tc.desc, func(t *testing.T) {
-			srv, ctx := setupMsgServer(t)
-
-			_, err := srv.CreateBaseledgerTransaction(ctx, &types.MsgCreateBaseledgerTransaction{Creator: creator})
-			require.NoError(t, err)
-			_, err = srv.DeleteBaseledgerTransaction(ctx, tc.request)
-			if tc.err != nil {
-				require.ErrorIs(t, err, tc.err)
-			} else {
-				require.NoError(t, err)
-			}
-		})
 	}
 }
