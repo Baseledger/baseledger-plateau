d3a1d9011dcaba63584c2fcb118ccb6d29f60fd9
ognjenkurtic <kurticognjen@gmail.com>
2022-02-22 16:59:26 +0100

    WIP Add orch keys setup to run testnet script

--- tests/clean.sh
+++ tests/clean.sh
@@ -5,6 +5,7 @@ set -eux
 # to be run with any PWD
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 VALIDATOR_CONTAINER_BASE_NAME="baseledger-validator-container"
+ETHEREUM_CONTAINER_NAME="baseledger-ethereum-node"
 NODES=3
 
 # Remove existing container instance
@@ -13,6 +14,8 @@ for i in $(seq 1 $NODES);
 do
 docker rm -f $VALIDATOR_CONTAINER_BASE_NAME$i
 done
+
+docker rm -f $ETHEREUM_CONTAINER_NAME
 
 docker network rm baseledgernet
 
