c201875f8030c4a80c18013a99a13ccffe56c50c
Stefan Kostic <skostic9242@gmail.com>
2022-02-22 23:06:15 +0100

    Extend validator power change test to check add, increase and decrease

--- e2e/test/baseledger.test.js
+++ e2e/test/baseledger.test.js
@@ -15,9 +15,8 @@ const sleep = (ms) => {
 const TEST_TIMEOUT = 30000;
 
 describe('validator power update', () => {
-  // TODO: test to add validator only 
-
-  it('should update validator staking power', async () => {
+  it('should add/update validator staking power', async function() {
+    this.timeout(TEST_TIMEOUT + 45000);
     const orchestratorValidatorResponse = await request(starport_url).get('/Baseledger/baseledger/bridge/orchestrator_validator_address')
         .send().expect(200);
 
@@ -33,30 +32,56 @@ describe('validator power update', () => {
 
     console.log('validator tokens ', parsedResponse.validator.tokens);
             
-    // console.log('ABI ', baseledger_abi)
-    // console.log('WEB3 ', await web3.eth.getAccounts());
+    // add payee
     const web3 = new Web3('http://localhost:8545');
     let contract = new web3.eth.Contract(baseledger_abi, "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512");
 
     const accounts = await web3.eth.getAccounts()
-    contract.methods.updatePayee(accounts[0], accounts[0], 100000010, validatorAddress).send({
+    contract.methods.addPayee(accounts[1], accounts[1], 100000100, validatorAddress).send({
         from: accounts[0]
     }).then(console.log)
 
+    await sleep(15000);
 
-    accountBalance = await request(starport_url).get(`/cosmos/bank/v1beta1/balances/${baseledgerAddress}`)
+    validators = await request(starport_url).get(`/cosmos/staking/v1beta1/validators/${validatorAddress}`)
     .send().expect(200);
 
-    parsedResponse = JSON.parse(accountBalance.text);
+    parsedResponse = JSON.parse(validators.text);
 
-    console.log('parsed response ', parsedResponse);
-    console.log('stake token balance ', parsedResponse.balances[0].amount);
-    console.log('work token balance ', parsedResponse.balances[1].amount);
+    console.log('validator tokens ', parsedResponse.validator.tokens);
+    expect(parsedResponse.validator.tokens).to.be.equal("100000100");
 
-    // TODO: check staking power before and after
+    // update payee, increase power
+    contract.methods.updatePayee(accounts[1], accounts[1], 100000200, validatorAddress).send({
+        from: accounts[0]
+    }).then(console.log)
+
+    await sleep(15000);
+
+    validators = await request(starport_url).get(`/cosmos/staking/v1beta1/validators/${validatorAddress}`)
+    .send().expect(200);
+
+    parsedResponse = JSON.parse(validators.text);
+
+    console.log('validator tokens ', parsedResponse.validator.tokens);
+    expect(parsedResponse.validator.tokens).to.be.equal("100000200");
+
+    // update payee, decrease power
+    contract.methods.updatePayee(accounts[1], accounts[1], 100000150, validatorAddress).send({
+        from: accounts[0]
+    }).then(console.log)
+
+    await sleep(15000);
+
+    validators = await request(starport_url).get(`/cosmos/staking/v1beta1/validators/${validatorAddress}`)
+    .send().expect(200);
+
+    parsedResponse = JSON.parse(validators.text);
+
+    console.log('validator tokens ', parsedResponse.validator.tokens);
+    expect(parsedResponse.validator.tokens).to.be.equal("100000150");
   });
 
-  // TODO: test to decrease pow
 });
 
 describe('ubt deposit', () => {
