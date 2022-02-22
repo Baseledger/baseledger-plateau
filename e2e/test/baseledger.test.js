c9a95218df53aeff47432dd12f2d386659dd0757
Stefan Kostic <skostic9242@gmail.com>
2022-02-22 22:09:39 +0100

    Add test for ubt deposit

--- e2e/test/baseledger.test.js
+++ e2e/test/baseledger.test.js
@@ -7,45 +7,94 @@ const starport_url = 'localhost:1317';
 
 const baseledger_abi = JSON.parse(fs.readFileSync(path.join(__dirname, 'baseledger_abi.json')));
 
-describe('test http', () => {
-    it ('should send bank request', async () => {
-        const orchestratorValidatorResponse = await request(starport_url).get('/Baseledger/baseledger/bridge/orchestrator_validator_address')
-            .send().expect(200);
+const sleep = (ms) => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
+const TEST_TIMEOUT = 30000;
 
-        // TODO: is there a better way to get some sample accounts? these should be as good as any others
-        const parsedOrchValResponse = JSON.parse(orchestratorValidatorResponse.text);
-        const baseledgerAddress = parsedOrchValResponse.orchestratorValidatorAddress[0].orchestratorAddress;
-        const validatorAddress = parsedOrchValResponse.orchestratorValidatorAddress[0].validatorAddress;
+describe('validator power update', () => {
+  // TODO: test to add validator only 
 
-        const testBankResponse = await request(starport_url).get(`/cosmos/bank/v1beta1/balances/${baseledgerAddress}`)
+  it('should update validator staking power', async () => {
+    const orchestratorValidatorResponse = await request(starport_url).get('/Baseledger/baseledger/bridge/orchestrator_validator_address')
         .send().expect(200);
 
-        const parsedResponse = JSON.parse(testBankResponse.text);
+    // TODO: is there a better way to get some sample accounts? these should be as good as any others
+    const parsedOrchValResponse = JSON.parse(orchestratorValidatorResponse.text);
+    const baseledgerAddress = parsedOrchValResponse.orchestratorValidatorAddress[0].orchestratorAddress;
+    const validatorAddress = parsedOrchValResponse.orchestratorValidatorAddress[0].validatorAddress;
 
-        console.log('parsed response ', parsedResponse);
+    let accountBalance = await request(starport_url).get(`/cosmos/bank/v1beta1/balances/${baseledgerAddress}`)
+    .send().expect(200);
 
-        console.log('stake token balance ', parsedResponse.balances[0].amount);
-        console.log('work token balance ', parsedResponse.balances[1].amount);
-                
-        // console.log('ABI ', baseledger_abi)
-        // console.log('WEB3 ', await web3.eth.getAccounts());
-        const web3 = new Web3('http://localhost:8545');
-        let contract = new web3.eth.Contract(baseledger_abi, "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512");
+    let parsedResponse = JSON.parse(accountBalance.text);
 
-        const accounts = await web3.eth.getAccounts()
-        contract.methods.updatePayee(accounts[0], accounts[0], 100000010, validatorAddress).send({
-            from: accounts[0]
-        }).then(console.log)
+    console.log('parsed response ', parsedResponse);
+
+    console.log('stake token balance ', parsedResponse.balances[0].amount);
+    console.log('work token balance ', parsedResponse.balances[1].amount);
+            
+    // console.log('ABI ', baseledger_abi)
+    // console.log('WEB3 ', await web3.eth.getAccounts());
+    const web3 = new Web3('http://localhost:8545');
+    let contract = new web3.eth.Contract(baseledger_abi, "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512");
+
+    const accounts = await web3.eth.getAccounts()
+    contract.methods.updatePayee(accounts[0], accounts[0], 100000010, validatorAddress).send({
+        from: accounts[0]
+    }).then(console.log)
 
 
-        const testBankResponse2 = await request(starport_url).get(`/cosmos/bank/v1beta1/balances/${baseledgerAddress}`)
-        .send().expect(200);
+    accountBalance = await request(starport_url).get(`/cosmos/bank/v1beta1/balances/${baseledgerAddress}`)
+    .send().expect(200);
 
-        const parsedResponse2 = JSON.parse(testBankResponse2.text);
+    parsedResponse = JSON.parse(accountBalance.text);
 
-        console.log('parsed response ', parsedResponse2);
-        console.log('stake token balance ', parsedResponse2.balances[0].amount);
-        console.log('work token balance ', parsedResponse2.balances[1].amount);
-        // console.log('CONTRACT ', contract.methods);
-    });
+    console.log('parsed response ', parsedResponse);
+    console.log('stake token balance ', parsedResponse.balances[0].amount);
+    console.log('work token balance ', parsedResponse.balances[1].amount);
+    // console.log('CONTRACT ', contract.methods);
+
+    // TODO: check staking power before and after
+  });
+
+  // TODO: test to decrease power
+});
+
+describe('ubt deposit', () => {
+  it('should deposit ubt to baseledger account', async function () {
+    this.timeout(TEST_TIMEOUT + 20000);
+    // TODO: how to get/generate account?
+    const baseledgerAddress = "baseledger1xu5xhzj63ddw7pce4r5d0y3w3fuzjxtylzvucm"
+
+    let accountBalance = await request(starport_url).get(`/cosmos/bank/v1beta1/balances/${baseledgerAddress}`)
+    .send().expect(200);
+
+    let parsedResponse = JSON.parse(accountBalance.text);
+
+    console.log('parsed response ', parsedResponse);
+
+    console.log('stake token balance ', parsedResponse.balances[0].amount);
+    console.log('work token balance ', parsedResponse.balances[1].amount);
+
+    const web3 = new Web3('http://localhost:8545');
+    let contract = new web3.eth.Contract(baseledger_abi, "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512");
+
+    const accounts = await web3.eth.getAccounts()
+    contract.methods.deposit(100, baseledgerAddress).send({
+        from: accounts[0]
+    }).then(console.log)
+
+    await sleep(20000)
+
+    accountBalance = await request(starport_url).get(`/cosmos/bank/v1beta1/balances/${baseledgerAddress}`)
+    .send().expect(200);
+
+    parsedResponse = JSON.parse(accountBalance.text);
+
+    console.log('parsed response ', parsedResponse);
+
+    console.log('stake token balance ', parsedResponse.balances[0].amount);
+    console.log('work token balance ', parsedResponse.balances[1].amount);
+  });
 });
