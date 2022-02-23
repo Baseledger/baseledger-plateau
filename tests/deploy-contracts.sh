a3d453dcf2e473f05b31a850313641a724715872
ognjenkurtic <kurticognjen@gmail.com>
2022-02-24 00:24:42 +0100

    Remove compile from deploy contracts. Add trace logging for orchestrator

--- tests/deploy-contracts.sh
+++ tests/deploy-contracts.sh
@@ -3,7 +3,6 @@ set -eux
 
 ETHEREUM_CONTAINER_NAME="baseledger-ethereum-node"
 
-docker exec --workdir /usr/src/app $ETHEREUM_CONTAINER_NAME npm run compile
 docker exec --workdir /usr/src/app $ETHEREUM_CONTAINER_NAME npm run contracts:migrate:local
 
 done
