5aa813c60e5d867bb3d1b23e78909592f8df45ba
Stefan Kostic <skostic9242@gmail.com>
2022-03-14 13:58:38 +0100

    Fix up keyring pass repeat

--- baseledger/x/proof/client/rest/txutil.go
+++ baseledger/x/proof/client/rest/txutil.go
@@ -85,7 +85,7 @@ func getKey(keyringInstance keyring.Keyring) (keyring.Info, error) {
 func NewKeyringInstance() (keyring.Keyring, error) {
 	input := &bytes.Buffer{}
 	fmt.Printf("KEYRING KEYRING KEYRING %v %v\n", viper.GetString("KEYRING_PASSWORD"), viper.GetString("KEYRING_DIR"))
-	fmt.Fprintln(input, viper.GetString("KEYRING_PASSWORD"))
+	fmt.Fprintln(input, viper.GetString("KEYRING_PASSWORD"), viper.GetString("KEYRING_PASSWORD"))
 	kr, err := keyring.New("baseledger", "file", viper.GetString("KEYRING_DIR"), input)
 
 	if err != nil {
