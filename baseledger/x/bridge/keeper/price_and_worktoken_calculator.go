f9ff09eb38ef553d6b77d5fd35a82f6a34e8a75c
ognjenkurtic <kurticognjen@gmail.com>
2022-03-14 20:52:16 +0100

    Change amount of decimals in worktoken price and amount calc

--- baseledger/x/bridge/keeper/price_and_worktoken_calculator.go
+++ baseledger/x/bridge/keeper/price_and_worktoken_calculator.go
@@ -17,13 +17,17 @@ func CalculateAvgUbtPriceForAttestation(att types.Attestation) *big.Int {
 }
 
 func CalculateAmountOfWorkTokens(worktokenEurPrice *big.Int, depositedUbtAmount *big.Int, averagePrice *big.Int) *big.Int {
+	// worktoken eur price in big int is 18 decimals and ubt token is 8 decimals, 
+	// so we divide by 10000000000 to remove unnecessary zeroes
+	worktokenEurPrice8decimals := new(big.Int).Quo(worktokenEurPrice, big.NewInt(10000000000))
+
 	depositedEurValueInt := depositedUbtAmount.Mul(depositedUbtAmount, averagePrice)
 
-	amountOfWorkTokens := new(big.Int).Quo(depositedEurValueInt, worktokenEurPrice)
+	amountOfWorkTokens := new(big.Int).Quo(depositedEurValueInt, worktokenEurPrice8decimals)
 
 	amountOfWorkTokensCeiled := ceilAmount(amountOfWorkTokens)
 
-	return new(big.Int).Div(amountOfWorkTokensCeiled, big.NewInt(1000000000000000000))
+	return new(big.Int).Div(amountOfWorkTokensCeiled, big.NewInt(100000000))
 }
 
 func calcMean(prices []sdk.Int) *big.Int {
@@ -83,12 +87,12 @@ func calcAvgPrice(prices []big.Int) *big.Int {
 }
 
 func ceilAmount(amount *big.Int) *big.Int {
-	one := big.NewInt(1000000000000000000)
-	remainder := new(big.Int).Mod(amount, one)
+	oneUbt := big.NewInt(100000000)
+	remainder := new(big.Int).Mod(amount, oneUbt)
 
 	if remainder.Cmp(big.NewInt(0)) == +1 {
 		amount.Sub(amount, remainder)
-		amount.Add(amount, one)
+		amount.Add(amount, oneUbt)
 	}
 
 	return amount
