7f19479cc19fd5e0ea27d5109204e80f57339873
ognjenkurtic <kurticognjen@gmail.com>
2022-03-17 15:44:25 +0100

    Add mainet folder with docs

---
+++ mainnet/v1.0.0/docs/prereqs.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+echo "Installing Golang"
+wget https://golang.org/dl/go1.16.5.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.16.5.linux-amd64.tar.gz
+chown -R root:root /usr/local/go
+mkdir -p $HOME/go/{bin,src}
+echo "export GOPATH=$HOME/go" >> ~/.profile 
+echo "export PATH=$PATH:$GOPATH/bin" >> ~/.profile
+echo "export PATH=$PATH:$GOPATH/bin:/usr/local/go/bin" >> ~/.profile
+. ~/.profile
+
+echo "Installing Cosmovisor"
+
+go install github.com/cosmos/cosmos-sdk/cosmovisor/cmd/cosmovisor@v1.0.0
+export DAEMON_HOME=$HOME/.baseledger
+export DAEMON_NAME=baseledgerd
+
+echo "Creating Cosmovisor folders"
+
+mkdir -p $DAEMON_HOME/cosmovisor/genesis
+mkdir -p $DAEMON_HOME/cosmovisor/genesis/bin
+mkdir -p $DAEMON_HOME/cosmovisor/upgrades
+mkdir -p $DAEMON_HOME/data
+
+echo "Moving baseledgerd binary to $DAEMON_HOME/cosmovisor/genesis/bin"
+
+tar -C $DAEMON_HOME/cosmovisor/genesis/bin -xzf baseledger_linux_amd64.tar.gz
+
+echo "Execute permission on baseledgerd binary"
+
+chmod +x $DAEMON_HOME/cosmovisor/genesis/bin/baseledgerd
+
+echo "Creating Orchestrator folders"
+
+mkdir -p $DAEMON_HOME/orchestrator
+
+echo "Moving baseledger_bridge binary to $DAEMON_HOME/orchestrator"
+
+mv ../orchestrator/baseledger_bridge $DAEMON_HOME/orchestrator 
+
+echo "Execute permission on baseledger_bridge binary"
+
+chmod +x $DAEMON_HOME/orchestrator/baseledger_bridge
